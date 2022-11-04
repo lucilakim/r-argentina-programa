5e5b4949acc595104ab1cddd7773137b45baa96d
Lucila Kim <lucila.kim.9@gmail.com>
2022-11-03 21:50:34 -0300

    Break the code into functions to subdivide the problem
    The problem was subdivided into 3 functions.
    
    The main one, which triggers the whole event.
    
    One that creates the inputs according to the number of classes entered by the user.
    
    One that calculates the hours, minutes and seconds and returns them in an array.

--- introduccion-a-js-master/tareas/clase-5/Resuelto/task-3_scripts.js
+++ introduccion-a-js-master/tareas/clase-5/Resuelto/task-3_scripts.js
@@ -2,13 +2,50 @@ const $buttonNumberClasses = document.querySelector("#enter-classes");
 
 $buttonNumberClasses.onclick = function () {
     const numberOfClasses = Number(document.querySelector("#number-of-classes").value);
+    const classes = createInputClasses(numberOfClasses);
+
+    // ----------------------------
+    // Creating a button to calculate the time of videos
+    // <button type="button" id="calculate-time">Calculate</button>
+    const calculateTimeButton = document.createElement('button');
+    calculateTimeButton.type = "button";
+    calculateTimeButton.id = "calculate-time";
+    calculateTimeButton.style.marginTop = "30px";
+    calculateTimeButton.style.display = "block";
+    calculateTimeButton.innerText = "Calculate";
+
+    classes.appendChild(calculateTimeButton); // Adding button to form
+
+    // ==============================
+    //Calculating time 
+    calculateTimeButton.onclick = function () {
+        const totalVideoTime = calculateDuration();
+        const FIRST_POSITION = 0;
+        const SECOND_POSITION = 1;
+        const THIRD_POSITION = 2;
+        const totalHours = totalVideoTime[FIRST_POSITION];
+        const totalMinutes = totalVideoTime[SECOND_POSITION];
+        const totalSeconds = totalVideoTime[THIRD_POSITION];
+
+        // ----------------------------
+        // Entering the total time of the videos in the dom
+        const $nodePageBody = document.querySelector('body');
+        const totalTime = document.createElement('h3');
+        totalTime.innerText = `The total time of the videos is: Hours: ${totalHours}, Minutes: ${totalMinutes}, Seconds: ${totalSeconds}.`
+        $nodePageBody.appendChild(totalTime);
+    }
+}
+
+
+
+function createInputClasses(quantity) {
     const $nodePageBody = document.querySelector('body');
     const div = document.createElement('div');
     const newTimeForm = document.createElement('form');
     div.appendChild(newTimeForm);
     $nodePageBody.appendChild(newTimeForm);
 
-    for (let i = 0; i < numberOfClasses; i++) {
+    for (let i = 0; i < quantity; i++) {
         // Creating input title
         // <h3>Class 1</h3>
         const inputTitle = document.createElement('h3');
@@ -34,7 +71,6 @@ $buttonNumberClasses.onclick = function () {
         const minutesLabel = document.createElement('label');
         minutesLabel.htmlFor = "class-minutes";
         minutesLabel.innerText = "Minutes: "
-
 
         //// Creating minutes input  
         //<input type="number" class="class-minutes" placeholder="Ej.: 25"/>
@@ -73,62 +109,45 @@ $buttonNumberClasses.onclick = function () {
         newTimeForm.appendChild(secondsInput)
     }
 
-    // ----------------------------
-    // Creating a button to calculate the time of videos
-    // <button type="button" id="calculate-time">Calculate</button>
-    const calculateTimeButton = document.createElement('button');
-    calculateTimeButton.type = "button";
-    calculateTimeButton.id = "calculate-time";
-    calculateTimeButton.style.marginTop = "30px";
-    calculateTimeButton.style.display = "block";
-    calculateTimeButton.innerText = "Calculate";
-
-    newTimeForm.appendChild(calculateTimeButton); // Adding button to dom
-
-    // ==============================
-    //Calculating time 
-    calculateTimeButton.onclick = function () {
-        const MINUTES_IN_SECONDS = 60;
-        //Obtaining the form inputs
-        const classHours = document.querySelectorAll(".class-hours");
-        const classMinutes = document.querySelectorAll(".class-minutes");
-        const classSeconds = document.querySelectorAll(".class-seconds");
-
-        let totalHours = 0;
-        let totalMinutes = 0;
-        let totalSeconds = 0;
-
-        // Calculating the total hours, minutes, and seconds of the classes
-        for (let i = 0; i < classHours.length; i++) {
-            totalHours += Number(classHours[i].value);
-            totalMinutes += Number(classMinutes[i].value);
-            totalSeconds += Number(classSeconds[i].value);
-        }
-
-        // if the seconds exceeds the second
-        if (totalSeconds >= MINUTES_IN_SECONDS) {
-            let fractionsOfSeconds = totalSeconds / MINUTES_IN_SECONDS;   // Obtaining amount of minutes to be added in minutes
-            let seconds = parseInt(fractionsOfSeconds) * MINUTES_IN_SECONDS; // Obtaining the leftover seconds
-            totalSeconds = totalSeconds - seconds; // Obtanining the final seconds
-
-            totalMinutes += parseInt(fractionsOfSeconds); // Adding the remaining seconds to the minutes
-        }
-
-        // if the minutes exceeds the minute
-        if (totalMinutes >= MINUTES_IN_SECONDS) {
-            let fractionsOfMinutes = totalMinutes / MINUTES_IN_SECONDS;   // Obtaining amount of minutes to be added in hours
-            let minutes = parseInt(fractionsOfMinutes) * MINUTES_IN_SECONDS; // Obtaining the leftover minutes
-            totalMinutes -= minutes; // Obtanining the final minutes
-
-            totalHours += parseInt(fractionsOfMinutes); // Adding the remaining minutes to the hours
-        }
-
-
-        // ----------------------------
-        // Entering the total time of the videos in the dom
-        const totalTime = document.createElement('h3');
-        totalTime.innerText = `The total time of the videos is: Hours: ${totalHours}, Minutes: ${totalMinutes}, Seconds: ${totalSeconds}.`
-        $nodePageBody.appendChild(totalTime);
-    }
+    return newTimeForm;
 }
 
+
+function calculateDuration() {
+    const MINUTES_IN_SECONDS = 60;
+    //Obtaining the form inputs
+    const classHours = document.querySelectorAll(".class-hours");
+    const classMinutes = document.querySelectorAll(".class-minutes");
+    const classSeconds = document.querySelectorAll(".class-seconds");
+
+    let totalHours = 0;
+    let totalMinutes = 0;
+    let totalSeconds = 0;
+
+    // Calculating the total hours, minutes, and seconds of the classes
+    for (let i = 0; i < classHours.length; i++) {
+        totalHours += Number(classHours[i].value);
+        totalMinutes += Number(classMinutes[i].value);
+        totalSeconds += Number(classSeconds[i].value);
+    }
+
+    // if the seconds exceeds the second
+    if (totalSeconds >= MINUTES_IN_SECONDS) {
+        let fractionsOfSeconds = totalSeconds / MINUTES_IN_SECONDS;   // Obtaining amount of minutes to be added in minutes
+        let seconds = parseInt(fractionsOfSeconds) * MINUTES_IN_SECONDS; // Obtaining the leftover seconds
+        totalSeconds = totalSeconds - seconds; // Obtanining the final seconds
+
+        totalMinutes += parseInt(fractionsOfSeconds); // Adding the remaining seconds to the minutes
+    }
+
+    // if the minutes exceeds the minute
+    if (totalMinutes >= MINUTES_IN_SECONDS) {
+        let fractionsOfMinutes = totalMinutes / MINUTES_IN_SECONDS;   // Obtaining amount of minutes to be added in hours
+        let minutes = parseInt(fractionsOfMinutes) * MINUTES_IN_SECONDS; // Obtaining the leftover minutes
+        totalMinutes -= minutes; // Obtanining the final minutes
+
+        totalHours += parseInt(fractionsOfMinutes); // Adding the remaining minutes to the hours
+    }
+
+    return [totalHours, totalMinutes, totalSeconds];
+}
